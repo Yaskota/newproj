662ec34e80d1a05642c5040eaad570e2ddf0b7bf
sunny-538 <ssanthoshpittala04@gmail.com>
2024-11-20 15:41:13 +0530

    cpp

---
+++ basic.cpp
@@ -0,0 +1,28 @@
+#include <iostream>  // Include the iostream library for input/output operations
+
+using namespace std;
+
+int main() {
+    // Declare and initialize variables
+    int number;
+    
+    // Output a message to the user
+    cout << "Enter a number: ";
+    
+    // Get input from the user
+    cin >> number;
+    
+    // Output the number entered by the user
+    cout << "You entered: " << number << endl;
+    
+    // A simple loop that prints numbers from 1 to 5
+    cout << "Counting from 1 to 5:" << endl;
+    for (int i = 1; i <= 5; i++) {
+        cout << i << " ";
+    }
+    
+    cout << endl; // Move to the next line
+
+    return 0;  // End of the program
+}
+
